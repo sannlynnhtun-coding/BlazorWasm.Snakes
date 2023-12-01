46afccca1ad8d5c95ac6dbc548bb8e5f671a4bda
MyatThitLwin <myatthitlwin@acedatasystems.com>
2023-12-01 22:32:05 +0630

    Added DataFrame and Service

---
+++ BlazorWasm.Snakes/Models/SnakeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWasm.Snakes.Models
+{
+    public class SnakeModel
+    {
+        public int Id { get; set; }
+        public string MMName { get; set; }
+        public string EngName { get; set; }
+        public string Detail { get; set; }
+        public string IsPoison { get; set; }
+        public string IsDanger { get; set; }
+    }
+
+}
--- BlazorWasm.Snakes/Program.cs
+++ BlazorWasm.Snakes/Program.cs
@@ -1,10 +1,12 @@
 using BlazorWasm.Snakes;
+using BlazorWasm.Snakes.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<SnakeService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
